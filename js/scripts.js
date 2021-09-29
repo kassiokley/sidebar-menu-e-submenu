ee9745c31a82aa08d7355fcb5d80d72b50f4fdd2
kassiokley <seloseishiro@gmail.com>
2021-09-29 01:41:03 -0300

    refatora funçao showMenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 /*Expandir o Menu*/
-const showMenu = (toggleId, navbarId) => {
+const showMenu = (toggleId, navbarId, bodyId) => {
   const toggle = document.getElementById(toggleId),
-  navbar = document.getElementById(navbarId)
+  navbar = document.getElementById(navbarId),
+  bodyPadding = document.getElementById(bodyId)
 
   if(toggle && navbar) {
     toggle.addEventListener('click', () => {
       navbar.classList.toggle('expandir')
+      bodyPadding.classList.toggle('body-pd')
     })
   }
 }
 
-showMenu('nav-toggle', 'navbar')
+showMenu('nav-toggle', 'navbar', 'body-pd')
